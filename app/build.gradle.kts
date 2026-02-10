4714e005f8ff3bf6da8df3b2a1679542b2788b50
Elnix90 <elnix91@gmail.com>
2026-02-10 21:43:54 +0100

    Update 2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "org.elnix.dragonlauncher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "2.4.1"
+        versionCode = 37
+        versionName = "2.5.0"
     }
 
 
