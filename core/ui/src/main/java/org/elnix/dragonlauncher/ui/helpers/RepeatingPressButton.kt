f896f9c13dda9261d70f3d914d158c4f7c1cf7a2
Elnix90 <elnix91@gmail.com>
2026-02-16 16:50:00 +0100

    Fixed RepeatingPressButton.kt using interaction source and rememberUpdatedState

--- core/ui/src/main/java/org/elnix/dragonlauncher/ui/helpers/RepeatingPressButton.kt
+++ core/ui/src/main/java/org/elnix/dragonlauncher/ui/helpers/RepeatingPressButton.kt
@@ -1,12 +1,15 @@
 package org.elnix.dragonlauncher.ui.helpers
 
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -17,6 +20,7 @@ import org.elnix.dragonlauncher.common.utils.vibrate
 @Composable
 fun RepeatingPressButton(
     enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = MutableInteractionSource(),
     intervalMs: Long = 70L,
     startDelayMs: Long = 300L,
     vibrate: Long = startDelayMs/2,
@@ -25,18 +29,19 @@ fun RepeatingPressButton(
 ) {
     val ctx = LocalContext.current
     var isPressed by remember { mutableStateOf(false) }
+    val currentOnPress by rememberUpdatedState(onPress)
 
     LaunchedEffect(isPressed, enabled) {
         if (enabled && isPressed) {
             // Immediate single press action
-            onPress()
+            currentOnPress()
 
             // Wait before repeat begins
             delay(startDelayMs)
 
             // If still pressed after delay, begin repeating
             while (isPressed) {
-                onPress()
+                currentOnPress()
                 if (vibrate > 0) vibrate(ctx, vibrate)
                 delay(intervalMs)
             }
@@ -49,9 +54,14 @@ fun RepeatingPressButton(
 
             detectTapGestures(
                 onPress = {
+                    val press = PressInteraction.Press(it)
+                    interactionSource.emit(press)
+
                     isPressed = true
                     tryAwaitRelease()
                     isPressed = false
+
+                    interactionSource.emit(PressInteraction.Release(press))
                 }
             )
         }
